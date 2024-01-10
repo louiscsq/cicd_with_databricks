42983e322d6cb66a0e86b8447e97a2566b6effa4
louis.chen@databricks.com <louis.chen@databricks.com>
2024-01-10 05:31:44 +0000

    Test github action

--- src/main/tests/integration_suite/test_integration_gold_layer_etl.py
+++ src/main/tests/integration_suite/test_integration_gold_layer_etl.py
@@ -48,3 +48,7 @@ assert query2.select("total_sales").collect()[0].total_sales > 0.0
 assert query3.columns == ["product_id","product_category","total_sales"]
 assert query4.select("total_customers").filter("state = 'Utah'").collect()[0].total_customers > 0.0
 assert query5.select("avg_sales").filter((col('year') == '2022') & (col('month') == '10')).collect()[0].avg_sales > 50.0
+
+# COMMAND ----------
+
+# test
